5c9d1e76786db0830acfa62bfbf36376c0fca376
Azul <mail@azulinho.com>
2017-03-05 23:25:46 +0000

    add update.sh script

---
+++ update.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+sudo rm -rf /old-root
+sudo rm -f /etc/nixos/result
+which git || sudo nix-env -i git
+test -e /nixpkgs || sudo git clone https://github.com/Azulinho/mynixpkgs.git /nixpkgs
+cd /nixpkgs && sudo git pull && sudo git checkout local
+cd /tmp
+which wget || sudo nix-env -i wget
+sudo wget --no-check-certificate -c --header='Cookie: oraclelicense=accept-securebackup-cookie' http://download.oracle.com/otn-pub/java/jdk/8u111-b14/jdk-8u111-linux-x64.tar.gz || echo
+sudo nix-store --add-fixed sha256 jdk-8u111-linux-x64.tar.gz
+
+sudo wget --no-check-certificate -c --header='Cookie: oraclelicense=accept-securebackup-cookie' http://download.oracle.com/otn-pub/java/jdk/8u121-b13/e9e7ea248e2c4826b92b3f075a80e441/jdk-8u121-linux-x64.tar.gz || echo
+sudo nix-store --add-fixed sha256 jdk-8u121-linux-x64.tar.gz
+
+
+if [ -e /swapfile ]; then
+    sudo swapon /swapfile
+else
+    sudo dd if=/dev/zero of=/swapfile bs=1M count=1024
+    sudo mkswap /swapfile
+    sudo swapon /swapfile
+fi
+
+set -e
+sudo nixos-rebuild build  -I nixpkgs=/nixpkgs/
+sudo nixos-rebuild switch  -I nixpkgs=/nixpkgs/
+sudo nix-collect-garbage -d
+
+set +e
+sudo swapoff /swapfile
+sudo rm -f /swapfile
+
