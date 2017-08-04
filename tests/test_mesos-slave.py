a271ac6fad429cd8a0ff934274b9522b4a94338d
Azul <mail@azulinho.com>
2017-08-04 18:16:43 +0100

    tests: fixes for mesos-slaves tests

--- tests/test_mesos-slave.py
+++ tests/test_mesos-slave.py
@@ -2,19 +2,19 @@ import pytest
 
 @pytest.mark.mesos_slave
 def test_mesos_slave_is_running(Command):
-    cmd = Command('systemctl is-active mesos-slave')
+    cmd = Command('sudo systemctl is-active mesos-slave')
     assert cmd.rc == 0
 
-    cmd = Command('systemctl status mesos-slave')
+    cmd = Command('sudo systemctl status mesos-slave')
     assert cmd.rc == 0
 
 @pytest.mark.mesos_slave
 def test_mesos_slave_is_enabled(Command):
-    cmd = Command('systemctl is-enabled mesos-slave')
+    cmd = Command('sudo systemctl is-enabled mesos-slave')
     assert cmd.rc == 0
 
 @pytest.mark.mesos_slave
 def test_mesos_slave_is_listening_on_port_5051_on_tinc_interface(Command):
-    cmd = Command("netstat -nltp | egrep -E 'tcp *.*169.254.*:5051 *.*:.*LISTEN.*mesos-slave'")
+    cmd = Command("sudo netstat -nltp | egrep -E 'tcp *.*169.254.*:5051 *.*:.*LISTEN.*mesos-slave'")
     assert cmd.rc == 0
 
