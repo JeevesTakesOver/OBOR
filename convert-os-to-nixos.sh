1ba6bd95ffbffb9f23d7e40f92ad40a42b65ddc0
Azul <mail@azulinho.com>
2017-08-04 18:25:21 +0100

    conver-os-to-nixos: fixes

--- convert-os-to-nixos.sh
+++ convert-os-to-nixos.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
-curl -L https://raw.githubusercontent.com/elitak/nixos-infect/master/nixos-infect > nixos-infect
-chmod 755 nixos-infect
-mkdir tmp
-export TMPDIR=/root/tmp
-./nixos-infect
+mkdir -p /tmp-nixos
+chmod 777 /tmp-nixos
+export TMPDIR=/tmp-nixos
+for ta in 1 2 3 4 5 6 7 8 9 10
+do
+	userdel nixbld$ta || true
+done
+groupdel nixbld  || true
+curl -L https://raw.githubusercontent.com/elitak/nixos-infect/c6e0edb73b97602550e43547bfa64f3bc9e6fe91/nixos-infect > nixos-infect
+chmod 755 nixos-infect 
+NIX_CHANNEL=nixos-16.09 ./nixos-infect
