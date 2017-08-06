cfbfcf23a78d89cb8ea3943785e4976c682d0198
Azul <mail@azulinho.com>
2017-08-06 22:25:14 +0100

    Revert "give update.sh full paths"
    This reverts commit 660ce929b2b7a5c6199312ba9633bda8fd826c71.

--- update.sh
+++ update.sh
@@ -8,13 +8,13 @@ cd /nixpkgs && sudo git pull && sudo git checkout local_release_1703
 cd /tmp
 which wget || sudo nix-env -i wget
 sudo wget -q --no-check-certificate -c --header='Cookie: oraclelicense=accept-securebackup-cookie' http://download.oracle.com/otn-pub/java/jdk/8u111-b14/jdk-8u111-linux-x64.tar.gz || echo
-sudo /run/current-system/sw/bin/nix-store --add-fixed sha256 jdk-8u111-linux-x64.tar.gz
+sudo nix-store --add-fixed sha256 jdk-8u111-linux-x64.tar.gz
 
 sudo wget -q --no-check-certificate -c --header='Cookie: oraclelicense=accept-securebackup-cookie' http://download.oracle.com/otn-pub/java/jdk/8u121-b13/e9e7ea248e2c4826b92b3f075a80e441/jdk-8u121-linux-x64.tar.gz || echo
-sudo /run/current-system/sw/bin/nix-store --add-fixed sha256 jdk-8u121-linux-x64.tar.gz
+sudo nix-store --add-fixed sha256 jdk-8u121-linux-x64.tar.gz
 
 sudo wget -q --no-check-certificate -c --header='Cookie: oraclelicense=accept-securebackup-cookie' http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz || echo
-sudo /run/current-system/sw/bin/nix-store --add-fixed sha256 jdk-8u131-linux-x64.tar.gz
+sudo nix-store --add-fixed sha256 jdk-8u131-linux-x64.tar.gz
 
 
 if [ -e /swapfile ]; then
@@ -26,9 +26,9 @@ else
 fi
 
 set -e
-sudo /run/current-system/sw/bin/nixos-rebuild build  -I nixpkgs=/nixpkgs/
-sudo /run/current-system/sw/bin/nixos-rebuild switch  -I nixpkgs=/nixpkgs/
-sudo /run/current-system/sw/bin/nix-collect-garbage -d
+sudo nixos-rebuild build  -I nixpkgs=/nixpkgs/
+sudo nixos-rebuild switch  -I nixpkgs=/nixpkgs/
+sudo nix-collect-garbage -d
 
 set +e
 sudo swapoff /swapfile
