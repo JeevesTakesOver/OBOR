b9adb5eb30d761fadb208c5aa99146b6de9225e3
Azul <mail@azulinho.com>
2017-08-06 22:18:19 +0100

    be less verbose

--- update.sh
+++ update.sh
@@ -7,13 +7,13 @@ test -e /nixpkgs || sudo git clone https://github.com/Azulinho/mynixpkgs.git /ni
 cd /nixpkgs && sudo git pull && sudo git checkout local_release_1703
 cd /tmp
 which wget || sudo nix-env -i wget
-sudo wget --no-check-certificate -c --header='Cookie: oraclelicense=accept-securebackup-cookie' http://download.oracle.com/otn-pub/java/jdk/8u111-b14/jdk-8u111-linux-x64.tar.gz || echo
+sudo wget -q --no-check-certificate -c --header='Cookie: oraclelicense=accept-securebackup-cookie' http://download.oracle.com/otn-pub/java/jdk/8u111-b14/jdk-8u111-linux-x64.tar.gz || echo
 sudo nix-store --add-fixed sha256 jdk-8u111-linux-x64.tar.gz
 
-sudo wget --no-check-certificate -c --header='Cookie: oraclelicense=accept-securebackup-cookie' http://download.oracle.com/otn-pub/java/jdk/8u121-b13/e9e7ea248e2c4826b92b3f075a80e441/jdk-8u121-linux-x64.tar.gz || echo
+sudo wget -q --no-check-certificate -c --header='Cookie: oraclelicense=accept-securebackup-cookie' http://download.oracle.com/otn-pub/java/jdk/8u121-b13/e9e7ea248e2c4826b92b3f075a80e441/jdk-8u121-linux-x64.tar.gz || echo
 sudo nix-store --add-fixed sha256 jdk-8u121-linux-x64.tar.gz
 
-sudo wget --no-check-certificate -c --header='Cookie: oraclelicense=accept-securebackup-cookie' http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz || echo
+sudo wget -q --no-check-certificate -c --header='Cookie: oraclelicense=accept-securebackup-cookie' http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz || echo
 sudo nix-store --add-fixed sha256 jdk-8u131-linux-x64.tar.gz
 
 
