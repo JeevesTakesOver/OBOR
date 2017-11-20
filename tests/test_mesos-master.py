9ea3bb54e6728545af339f649142178dfbcbec11
Azul <mail@azulinho.com>
2017-11-20 20:34:51 +0000

    drop all hardcoded 169.254 references

--- tests/test_mesos-master.py
+++ tests/test_mesos-master.py
@@ -18,3 +18,9 @@ def test_mesos_master_is_listening_on_port_5050_on_tinc_interface(Command):
     cmd = Command("sudo netstat -nltp | egrep -E 'tcp *.*10.254.*:5050 *.*:.*LISTEN.*mesos-master'")
     assert cmd.rc == 0
 
+
+@pytest.mark.mesos_master
+def test_mesos_master_should_ping_mesos_master_nodes(Command):
+    for ip in ['10.254.0.11', '10.254.0.12', '10.254.0.13']:
+        cmd = Command("ping -c 1 %s" % ip)
+        assert cmd.rc == 0
