1f8cfa89933b1f383d3175b869303c1ba2d22a54
Azul <mail@azulinho.com>
2018-02-11 19:05:26 +0000

    bump parallel nix jobs and build-cores

--- common/nix.nix
+++ common/nix.nix
@@ -1,14 +1,14 @@
 { nix, ... }:
 
 {
-  nix.maxJobs = 4;
+  nix.maxJobs = 8;
   nix.useSandbox = true;
   nix.daemonNiceLevel = 19;
   # we cleanup through our update.sh script
   # nix.gc.automatic = false;
   # nix.gc.dates = "03:15";
   nix.extraOptions = ''
-    build-cores = 2
+    build-cores = 4
     gc-keep-outputs = true
   '';
 
