c4a7e79261f54c5b1f46dc63a921d96649abd54d
Azul <mail@azulinho.com>
2018-03-10 20:12:23 +0000

    retry up to 3 times the nixos rebuild step

--- update.sh
+++ update.sh
@@ -1,5 +1,24 @@
 #!/run/current-system/sw/bin/bash
 
+function retry {
+  local retry_max=$1
+  shift
+
+  local count=$retry_max
+  while [ $count -gt 0 ]; do
+    "$@" && break
+    count=$(($count - 1))
+    sleep 1
+  done
+
+  [ $count -eq 0 ] && {
+    echo "Retry failed [$retry_max]: $@" >&2
+    return 1
+  }
+  return 0
+}
+
+
 echo "running update.sh..."
 
 echo "update.sh: cleaning old files..."
@@ -40,14 +59,14 @@ echo "update.sh: making sure jdks are installed..."
 set -e
 echo "update.sh: running nixos-rebuild build..."
     # https://github.com/NixOS/nix/issues/443
-    sudo CURL_CA_BUNDLE=/etc/ca-bundle.crt nixos-rebuild build -Q -I nixpkgs=/nixpkgs/ 2>&1 | tail 
+    retry 3 sudo CURL_CA_BUNDLE=/etc/ca-bundle.crt nixos-rebuild build -Q -I nixpkgs=/nixpkgs/ 
 
 echo "update.sh: running nixos-rebuild switch..."
     # https://github.com/NixOS/nix/issues/443
-    sudo CURL_CA_BUNDLE=/etc/ca-bundle.crt nixos-rebuild switch -Q -I nixpkgs=/nixpkgs/ 2>&1 | tail
+    retry 3 sudo CURL_CA_BUNDLE=/etc/ca-bundle.crt nixos-rebuild switch -Q -I nixpkgs=/nixpkgs/
 
 echo "update.sh: cleaning old packages..."
-    sudo nix-collect-garbage -d >/dev/null 2>&1| tail
+    sudo nix-collect-garbage -d >/dev/null 
 
 set +e
 echo "finished update.sh..."
