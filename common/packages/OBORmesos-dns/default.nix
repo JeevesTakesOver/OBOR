25e598a749012f382201c27c66f22bba1c26a946
Azul <mail@azulinho.com>
2018-04-05 19:26:42 +0100

    update mesos-dns to latest version

--- common/packages/OBORmesos-dns/default.nix
+++ common/packages/OBORmesos-dns/default.nix
@@ -2,7 +2,7 @@
 
 buildGoPackage rec {
   name = "mesos-dns-${version}";
-  version = "0.1.2";
+  version = "0.6.0";
   rev = "v${version}";
   
   goPackagePath = "github.com/mesosphere/mesos-dns";
@@ -14,7 +14,7 @@ buildGoPackage rec {
     inherit rev;
     owner = "mesosphere";
     repo = "mesos-dns";
-    sha256 = "0zs6lcgk43j7jp370qnii7n55cd9pa8gl56r8hy4nagfvlvrcm02";
+    sha256 = "1p7ai51hy80pmsyi5y4hspyh2965rca42wg314051y8rz05l8j00";
   };
 
   goDeps = ./deps.nix;
