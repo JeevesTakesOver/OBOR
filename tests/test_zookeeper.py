ecff60ed27b72e8f32c07b2d95083617531a1b42
Azul <mail@azulinho.com>
2018-05-03 21:49:36 +0100

    zookeeper had wrong pytest mark

--- tests/test_zookeeper.py
+++ tests/test_zookeeper.py
@@ -1,6 +1,6 @@
 import pytest
 
-@pytest.mark.mesos_dns
+@pytest.mark.zookeeper
 def test_zookeeper_is_running(Command):
     cmd = Command('sudo systemctl is-active OBORzookeeper')
     if cmd.rc != 0:
@@ -10,13 +10,13 @@ def test_zookeeper_is_running(Command):
     if cmd.rc != 0:
         raise AssertionError()
 
-@pytest.mark.mesos_dns
+@pytest.mark.zookeeper
 def test_zookeeper_is_enabled(Command):
     cmd = Command('sudo systemctl is-enabled OBORzookeeper')
     if cmd.rc != 0:
         raise AssertionError()
 
-@pytest.mark.mesos_dns
+@pytest.mark.zookeeper
 def test_zookeeper_is_listening_on_port_2181_on_tinc_interface(Command):
     cmd = Command("sudo netstat -nltp | egrep -E 'tcp *.*10.254.*:2181 *.*:.*LISTEN.*java'")
     if cmd.rc != 0:
