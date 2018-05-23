6d82545ff50e5f594b5115025370af0c1f4b9643
Azul <mail@azulinho.com>
2018-05-23 21:21:23 +0000

    fix tests where boxes don't have nslookup

--- tests/test_dns_resolution.py
+++ tests/test_dns_resolution.py
@@ -13,6 +13,6 @@ def test_tinc_core_vpn_hostnames_are_resolvable( Command):
 
 @pytest.mark.dns_resolution
 def test_mesos_dns_should_resolve_leader_mesos(Command):
-    cmd = Command("nslookup leader.mesos")
+    cmd = Command("host leader.mesos")
     if cmd.rc != 0:
         raise AssertionError()
