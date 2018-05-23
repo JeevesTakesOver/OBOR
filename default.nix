6988712681afb168fe87c04521394cf3ba7a1b86
Azul <mail@azulinho.com>
2018-05-23 21:23:30 +0000

    add vagrant to default.nix

--- default.nix
+++ default.nix
@@ -1,8 +1,21 @@
-with import <nixpkgs> {};
-with pkgs.python27Packages;
+with import <nixpkgs> {
 
+overlays = [
+    (self: super: {
+      bundler = super.bundler.overrideAttrs (old: {
+        name = "bundler-1.16.1";
+        src = super.fetchurl {
+          url = "https://rubygems.org/gems/bundler-1.16.1.gem";
+          sha256 = "1s2nq4qnffxg3kwrk7cnwxcvfihlhxm9absl2l6d3qckf3sy1f22";
+        };
+      });
+    })
+  ];
+};
+
+with pkgs.python27Packages;
 stdenv.mkDerivation {
-  name = "impurePythonEnv";
+  name = "env";
   buildInputs = [
     # these packages are required for virtualenv and pip to work:
     #
@@ -22,6 +35,7 @@ stdenv.mkDerivation {
     zlib
     unzip
     libffi
+    vagrant
   ];
   src = null;
   shellHook = ''
